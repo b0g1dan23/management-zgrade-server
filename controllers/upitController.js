21ff0e83fc74709c10849c61e4d21f6cacfb9459
Bogdan Stevanovic <bogdan.stevanovic090204@gmail.com>
2023-05-22 22:49:53 +0200

    dodata zgrada i broj

--- controllers/upitController.js
+++ controllers/upitController.js
@@ -5,7 +5,7 @@ export const fetchUpiti = async (req, res) => {
     if (req.query.startDate && req.query.endDate) {
       const upiti = await Upit.find({
         createdAt: { $gte: req.query.startDate, $lte: req.query.endDate },
-      });
+      }).populate("idZgrade", ["ulica", "broj"]);
       res.send(upiti);
     } else {
       const upiti = await Upit.find()
